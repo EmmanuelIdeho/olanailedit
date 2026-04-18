279db49525597341a44700b14a8ece7a136d18bd
EmmanuelIdeho <ohis98@gmail.com>
2026-04-18 13:23:36 -0400

    Updated email settings

--- olanailedit/settings.py
+++ olanailedit/settings.py
@@ -87,10 +87,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool, default=False)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = config('EMAIL_USE_SSL', cast=bool, default=False)
 DEFAULT_FROM_EMAIL = f"OlaNailedit <{config('EMAIL_HOST_USER')}>"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
